3d5333763a19706a88c570a382c7e015a30b262d
Akram Potrebin <akram.potrebin@opencbs.com>
2017-05-02 17:28:48 +0600

    Задание 8

--- test_example.py
+++ test_example.py
@@ -2,23 +2,46 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    # wd = webdriver.Ie(executable_path="C:/Tools/IEDriverServer.exe")
+    # wd = webdriver.Firefox(firefox_profile = options, capabilities={"marionette": True}, executable_path="C:/Tools/geckodriver.exe", firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
+    # wd = webdriver.Firefox(executable_path="C:/Tools/geckodriver.exe", firefox_binary="C:/Program Files/Nightly/firefox.exe")
+
+    # ---------------- Chorme --------------------- #
+    # wd = webdriver.Chrome(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    options.add_argument("start-maximized")
+
+
+    wd = webdriver.Chrome(executable_path="C:/Tools/chromedriver.exe", chrome_options=options)
+    # print(wd.capabilities)
     wd.implicitly_wait(10)
     request.addfinalizer(wd.quit)
+
     return wd
 
 
-def test_login(driver):
-    driver.get("http://localhost:8080/litecart/admin/login.php")
-    wait = WebDriverWait(driver, 10)  # seconds
-    driver.find_element_by_name("username").send_keys("admin")
-    driver.find_element_by_name("password").send_keys("admin")
-    driver.find_element_by_xpath(".//*[@id='box-login']/form/div[2]/button").click()
-    driver.find_element_by_xpath(".//*[@id='shortcuts']/a[5]/i").click()
-    wait.until(EC.title_is("My Store"))
+def test_stickers_equal_products(driver):
+    driver.get('http://localhost:8082/litecart/en/')
+    all_products = driver.find_elements_by_css_selector('.col-xs-halfs.col-sm-thirds.col-md-fourths.col-lg-fifths')
+    all_stickers = driver.find_elements_by_css_selector('.sticker')
+    assert len(all_products) == len(all_stickers)
+
+
+
+
+
+
+
+
+
+
 
 
